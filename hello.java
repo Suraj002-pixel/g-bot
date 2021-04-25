462611d06411d65045d0a6dc3b1ac6142b1c201e
Ubuntu <ubuntu@ip-172-31-17-74.us-east-2.compute.internal>
2021-04-25 15:54:06 +0000

    added a new file readme.md and added a comment line

--- hello.java
+++ hello.java
@@ -1,3 +1,4 @@
+This is modified file
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
---
+++ readme.md
@@ -0,0 +1,4 @@
+This is just readme file
+
+DevA
+DevB
