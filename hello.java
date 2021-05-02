40dabd2f5a802cd1690fcd117301a07b2366bbbb
Ubuntu <ubuntu@ip-172-31-17-74.us-east-2.compute.internal>
2021-05-02 06:53:56 +0000

    added new code in abc branch

--- hello.java
+++ hello.java
@@ -1,6 +1,9 @@
-# This is modified file
-class HelloWorld {
+//This is a sample helloworld java file
+//Adding a new comment line
+class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        System.out.println("Welcome to GIT!");
+        System.out.println("Welcome to Branching");
     }
 }
